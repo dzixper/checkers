41ad36ca8fdae6b750fb1b1161c8bca923a06b3b
Szymon Olcen <dzixper@Szymons-MacBook-Pro.local>
2020-06-22 00:05:54 +0200

    Fully working beating

--- src/js/app.js
+++ src/js/app.js
@@ -11,7 +11,38 @@ let board = [
     [1, 0, 1, 0, 1, 0, 1, 0]    //1 = player1
 ];
 
-function initBoard() {
+let pieceChecked = false,
+    pieceColumn = 0,
+    pieceRow = 0,
+    tileColumn = 0,
+    tileRow = 0,
+    playerTurn = 1,
+    battleMode = false;
+
+function restart() {
+    board = [
+        [0, 2, 0, 2, 0, 2, 0, 2],   //2 = player2
+        [2, 0, 2, 0, 2, 0, 2, 0],
+        [0, 2, 0, 2, 0, 2, 0, 2],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [1, 0, 1, 0, 1, 0, 1, 0],
+        [0, 1, 0, 1, 0, 1, 0, 1],
+        [1, 0, 1, 0, 1, 0, 1, 0]    //1 = player1
+    ];
+    pieceChecked = false;
+    pieceColumn = 0;
+    pieceRow = 0;
+    tileColumn = 0;
+    tileRow = 0;
+    playerTurn = 1;
+    battleMode = false;
+    currentPlayer();
+    updateBoard();
+
+}
+
+function updateBoard() {
     document.getElementById('board').innerHTML = ''; //clear div
     for (let i = 0; i < 8; i++) {
         for (let j = 0; j < 8; j++) {
@@ -33,14 +64,7 @@ function initBoard() {
     }
 }
 
-initBoard();
-
-let pieceChecked = false,
-    pieceColumn = 0,
-    pieceRow = 0,
-    tileColumn = 0,
-    tileRow = 0,
-    playerTurn = 1;
+restart();
 
 function currentPlayer() {
     document.getElementById('turn').innerText = "Player " + playerTurn + " moves";
@@ -48,52 +72,68 @@ function currentPlayer() {
 
 function swapPlayer() {
     pieceChecked = false;
+    battleMode = false;
     playerTurn = (playerTurn === 1) ? 2 : 1;
     currentPlayer();
+}
+
+function otherPlayer() {
+    return (playerTurn === 1) ? 2 : 1;
 }
 
 function checkPiece() {
     if (playerTurn === 1 && this.classList.value === 'piece player2') return false;
     if (playerTurn === 2 && this.classList.value === 'piece player1') return false;
-    pieceColumn, pieceRow = 0; //TODO -- czy to jest potrzebne?
+    if (battleMode) return false;
     pieceChecked = true;
     [pieceRow, pieceColumn] = this.parentElement.id;
-    pieceRow = parseInt(pieceRow); //TODO -- zrobic to lepiej
+    pieceRow = parseInt(pieceRow); //TODO -- make it better
     pieceColumn = parseInt(pieceColumn);
 }
 
 function movePossible() {
     if (this.classList.value === 'tile white') return false;
-    if (!pieceChecked) return false; //TODO -- czy to jest potrzebne?
+    if (!pieceChecked) return false;
     if (this.hasChildNodes()) return false;
 
-    tileRow, tileColumn = 0; //TODO -- czy to jest potrzebne?
     [tileRow, tileColumn] = this.id;
-    tileRow = parseInt(tileRow); //TODO -- zrobic to lepiej
+    tileRow = parseInt(tileRow); //TODO -- make it better
     tileColumn = parseInt(tileColumn);
 
+    if (playerTurn === 1 && pieceRow - tileRow === 1 && Math.abs(pieceColumn - tileColumn) === 1 && !battleMode) {
+        move();
+        swapPlayer();
+    }
+    if (playerTurn === 2 && pieceRow - tileRow === -1 && Math.abs(pieceColumn - tileColumn) === 1 && !battleMode) {
+        move();
+        swapPlayer();
+    }
+
+    //if beating is possible
     if (Math.abs(pieceRow - tileRow) === 2 &&
         Math.abs(pieceColumn - tileColumn) === 2 &&
         board[tileRow][tileColumn] === 0 &&
-        board[(tileRow+pieceRow)/2][(tileColumn+pieceColumn)/2] !== 0) {
+        ((playerTurn === 1 && board[(tileRow+pieceRow)/2][(tileColumn+pieceColumn)/2] === 2) || //try otherPlayer()
+        (playerTurn === 2 && board[(tileRow+pieceRow)/2][(tileColumn+pieceColumn)/2] === 1))) {
+        battleMode = true;
         board[(tileRow + pieceRow) / 2][(tileColumn + pieceColumn) / 2] = 0
         move();
 
-    }
 
-    if (playerTurn === 1 && pieceRow - tileRow === 1 && Math.abs(pieceColumn - tileColumn) === 1) {
-        move();
-        swapPlayer();
-    }
-    if (playerTurn === 2 && pieceRow - tileRow === -1 && Math.abs(pieceColumn - tileColumn) === 1) {
-        move();
-        swapPlayer();
+        if ((pieceRow-2 >= 0 || pieceColumn-2 >= 0 || board[pieceRow-1][pieceColumn-1] !== otherPlayer() || board[pieceRow-2][pieceColumn-2] !== 0) &&
+            (pieceRow-2 >= 0 || pieceColumn+2 <= 7 || board[pieceRow-1][pieceColumn+1] !== otherPlayer() || board[pieceRow-2][pieceColumn+2] !== 0) &&
+            (pieceRow+2 <= 7 || pieceColumn-2 >= 0 || board[pieceRow+1][pieceColumn-1] !== otherPlayer() || board[pieceRow+2][pieceColumn-2] !== 0) &&
+            (pieceRow+2 <= 7 || pieceColumn+2 <= 7 || board[pieceRow+1][pieceColumn+1] !== otherPlayer() || board[pieceRow+2][pieceColumn+2] !== 0)) {
+            swapPlayer();
+        }
+
+
     }
 }
 
 function move() {
     [board[pieceRow][pieceColumn], board[tileRow][tileColumn]] = [board[tileRow][tileColumn], board[pieceRow][pieceColumn]]; //swap places
     [pieceRow, pieceColumn] = [tileRow, tileColumn];
-    initBoard();
+    updateBoard();
 }
 
