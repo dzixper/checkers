5d2291f014deb2dbbafb6dd289b3b6607793293f
Szymon Olcen <dzixper@Szymons-MacBook-Pro.local>
2020-06-22 01:13:16 +0200

    moze teraz?

--- src/js/app.js
+++ src/js/app.js
@@ -124,7 +124,6 @@ function movePossible() {
             (pieceRow-2 < 0 || pieceColumn+2 > 7 || board[pieceRow-1][pieceColumn+1] !== otherPlayer() || board[pieceRow-2][pieceColumn+2] !== 0) &&
             (pieceRow+2 > 7 || pieceColumn-2 < 0 || board[pieceRow+1][pieceColumn-1] !== otherPlayer() || board[pieceRow+2][pieceColumn-2] !== 0) &&
             (pieceRow+2 > 7 || pieceColumn+2 > 7 || board[pieceRow+1][pieceColumn+1] !== otherPlayer() || board[pieceRow+2][pieceColumn+2] !== 0)) {
-            console.log(board[pieceRow-1][pieceColumn+1]);
             swapPlayer();
         }
 
